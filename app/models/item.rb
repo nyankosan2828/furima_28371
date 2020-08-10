18fbee7c5761020296b8eb8add506b6bfea014e0
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-08-10 12:27:29 +0900

    Setting validation and Implementation of error messages

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :price, numericality:
+      { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+  end
+
+  with_options numericality: { message: 'select' } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_origin_id
+    validates :shipping_date_id
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
