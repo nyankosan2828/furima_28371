c08f34e3efd2147015b04227a5cf015c2cb73c3e
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-08-10 11:22:24 +0900

    Implementation of item listing function using active_storage

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :shipping_origin
+  belongs_to_active_hash :shipping_date
 end
