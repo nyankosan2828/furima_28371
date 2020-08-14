26a0c21f55c5c084207414f22aa07a23b26e1d5d
sawa8626 <68460801+sawa8626@users.noreply.github.com>
2020-08-14 16:33:40 +0900

    Setting access validation

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,15 @@
 class PurchasesController < ApplicationController
+  before_action :move_to_session
+  before_action :set_item
+  before_action :move_to_item_index
+
   # 商品購入ページ
   def index
-    @item = Item.find(params[:item_id])
     @purchase = PurchaseShipment.new
   end
 
   # 購入者情報の保存
   def create
-    @item = Item.find(params[:item_id])
     @purchase = PurchaseShipment.new(purchase_params)
     if @purchase.valid?
       pay_item(card_params)
@@ -20,6 +22,19 @@ class PurchasesController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_session
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
+  def move_to_item_index
+    redirect_to root_path unless current_user != @item.user
+    redirect_to root_path if @item.purchase_status
+  end
 
   def purchase_params
     params.require(:purchase_shipment).permit(:postal_code, :prefecture_id, :city, :street_number, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
